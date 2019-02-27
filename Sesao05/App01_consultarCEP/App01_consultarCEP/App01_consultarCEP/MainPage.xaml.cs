87fc029dd8f91e041764ac0781962651b20307c9
joaopaulogabarrao <joaopaulogabarrao@gmail.com>
2019-02-27 00:01:37 -0300

    criacao do primeiro aplicativo

---
+++ Sesao05/App01_consultarCEP/App01_consultarCEP/App01_consultarCEP/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using App01_consultarCEP.Servico.Modelo;
+using App01_consultarCEP.Servico;
+
+namespace App01_consultarCEP
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BOTAO.Clicked += BuscarCep;
+        }
+
+        private void BuscarCep(object sender, EventArgs e)
+        {
+            string cep = CEP.Text.Trim();
+            Endereco end = ViaCEPServico.BuscarEnderecoViaCEP(cep);
+
+            RESULTADO.Text = $"Endereco: {end.bairro} CEP: {end.cep}" +
+                            $"Rua: {end.logradouro} UF: {end.uf}";
+
+        }
+    }
+}
